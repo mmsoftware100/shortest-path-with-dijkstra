015c7a97fd4cc96be1f50fa719da1316ba0d5b3a
Aung Ko Man <aungkoman@gmail.com>
2024-05-13 10:11:54 +0630

    Update settings.py

--- sp/settings.py
+++ sp/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # this is new line to add
+    "sp.app.AppConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
