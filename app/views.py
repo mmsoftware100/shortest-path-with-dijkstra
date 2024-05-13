f3b386c661d854c2e451f16d1bc1edb47e444682
Aung Ko Man <aungkoman@gmail.com>
2024-05-13 10:12:51 +0630

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+# step 1.1 import HttpResponse 
+from django.http import HttpResponse
 
-# Create your views here.
+# step 1.2 create function with request parameter
+# index is function name
+def index(request):
+    # step 1.3 return content with HttpResponse
+    return HttpResponse("Hello World")
