e35807065d175e93094dadb42c1731d3feb02927
Aung Ko Man <aungkoman@gmail.com>
2024-05-13 10:13:11 +0630

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-
 from . import views
 
-urlpatterns = [ ]
+urlpatterns = [
+    # add first element to urlpatterns array
+    # path function accept two parameter
+    # first parameter is url path
+    # second parameter is function name 
+    path("hello/", views.index),
+]
